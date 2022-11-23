699e8dffd01576465e3bdedeb487c288dca22c90
Акрам <115643985+akramblch@users.noreply.github.com>
2022-11-23 19:25:35 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Mainhub from './components/Mainhub';
+import './scss/Mainhub.scss'
+
+
+function App() {
+  return (
+    <>
+      <Mainhub/>
+      <main>
+        <section></section>
+        <section></section>
+        <section></section>
+      </main>
+      <footer></footer>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Mainhub.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+const Mainhub = () => {
+  return (
+    <div>
+      <div className='hub'>
+        <div className='container'>
+          <div className='hub-content'>
+                <h1 className='hub-content-brand'>бренд</h1>
+                <h2 className='hub-content-vintage'>american vintage</h2>
+                <p className='hub-content-vintage-p'>American Vintage - модная торговая марка по производству трикотажной одежды для мужчин и женщин. </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Mainhub
